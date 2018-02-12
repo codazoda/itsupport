296c2f09e7e72aea5a41d683977b4200a25ed498
Joel Dare <joel@joeldare.com>
2018-02-12 11:14:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# IT Support
+
+This is a simple _IT Support_ Alexa skill.
+
+
+## Prompts
+
+_Alexa, Ask IT Support how to [setup the VPN]_
+
+Sends an email to it@deseretdigital.com with {something} in the body.
+
+
+## Notes
+
+This code is served from the following URL.
+
+https://www.joeldare.com/itsupport/
+
+Run this on the server to pull every 15 seconds while we're working.
+
+`watch -n15 "git pull"`
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+// Turn on all error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Setup some variables
+$validAppId = 'amzn1.ask.skill.535f89e1-f247-4ecf-9853-07a0414ea8b1';
+
+// Get raw POST data
+$postData = file_get_contents('php://input');
+
+// Decode the JSON
+$alexaRequest = json_decode($postData);
+
+// Verify the application ID
+if ($alexaRequest->session->application->applicationId === $validAppId) {
+
+	// Look at the session.request.intent.name
+	switch($alexaRequest->session->request->intent->name) {
+		'howto':
+			$response = itHowto();
+			break;
+		default:
+			$response = itUnknown();
+	}
+
+	// Setup a JSON response header and send the json response
+	header('Content-Type: application/json');
+	echo json_encode($response);
+
+}
+
+function itUnknown() {
+
+	// Setup a response
+	$response = [
+		"response" => [
+			"outputSpeech" => [
+				"type" => "SSML",
+				"ssml" => "<speak>Sorry, I.T. Support is confused.</speak>"
+			]
+		]
+	];
+
+	return $response;
+
+}
+
+function itHowto() {
+
+	// TODO: Grab the {message} from the request
+	// TODO: Email IT Support
+
+	// Setup a response
+	$response = [
+		"response" => [
+			"outputSpeech" => [
+				"type" => "SSML",
+				"ssml" => "<speak>Okay, I've emailed I.T. Support.</speak>"
+			]
+		]
+	];
+
+	return $response;
+
+}
